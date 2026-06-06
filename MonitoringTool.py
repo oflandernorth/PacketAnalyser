8083719c7f585997dad00434c9de4f1dcbb3d536
oflandernorth <oflandernorth@gmail.com>
2026-06-06 21:13:30 +0330

    Added country checking for IPs and a safe ip list

--- MonitoringTool.py
+++ MonitoringTool.py
@@ -1,12 +1,20 @@
 import sys
+import requests
 from scapy.all import *
 
 IPs = {}
+safelist = 0
 
 def main():  
+    global safelist
     print("Choose the type of operation you want to perform.")
-    OpType = input("live for live capture / read for capture file reading: ")
-    match (OpType):
+    opType = input("live for live capture / read for capture file reading: ")
+    safelist = input("if you want to check against a list of safe ips enter the txt files address: ")
+    if safelist != 0:
+        safelist = open(safelist)
+        safelist = safelist.read()
+        safelist = safelist.split("\n")
+    match (opType):
         case "live":
             print("starting live capture")
             liveCap()
@@ -18,6 +26,7 @@ def main():
             print("please enter a proper operation type \n\n")
             main()
             pass
+    ip_checker()
 def liveCap():
     #intf = input("which interface to sniff on: ")
     intf = "Wi-Fi"
@@ -26,19 +35,24 @@ def liveCap():
         case "time":
             timeO = int(input("how long to capture for: "))
             sniff(iface=intf, prn= analysis_func, store=False, filter="ip", timeout= timeO)
+            pass
         case "amount":
             counT = int(input("how many packets to capture: "))
             sniff(iface=intf, prn= analysis_func, store=False, filter="ip", count= counT)
+            pass
         case _:
             print("Choose between time and amount")
             liveCap()
             pass
+
 def analysis_func(pkt):
     if IP in pkt:
         src = pkt["IP"].src
         dst = pkt["IP"].dst
         proto = pkt["IP"].proto
         flags = 0
+        sport = 0
+        dport = 0
         match (proto):
             case 6:
                 sport = pkt["TCP"].sport
@@ -50,17 +64,36 @@ def analysis_func(pkt):
                 dport = pkt["UDP"].dport
                 pass
             case 1:
-                sport = pkt["ICMP"].sport
-                dport = pkt["ICMP"].dport
                 pass
             case _:
                 print(f"Unknown protocol packet from {src}")
                 pass
-        packetCheck(src, dst, proto, sport, dport, flags)
-    #print(f"source = {src}, destination = {dst}, protocol = {proto}, sport = {sport}, dport = {dport}, flags = {flags}")
-def packet_check(src, dst, proto, sport, dport, flags):
-    print(flags)
-if sys.argv[1] == "live":
-    liveCap()
-elif __name__ == "__main__":
-    main()
+        
+        #print(f"source = {src}, destination = {dst}, protocol = {proto}, sport = {sport}, dport = {dport}, flags = {flags}")
+        packet_to_list(src, dst, proto, sport, dport, flags)
+
+def packet_to_list(src, dst, proto, sport, dport, flags):
+    global IPs
+    if src in safelist:
+        pass
+    else:
+        if src not in IPs:
+            IPs.update({src: {"proto": proto, "dport": dport, "flags": flags, "count" : 0}})
+        else:
+            count = IPs[src]["count"]+1
+            IPs.update({src: {"proto": proto, "dport": dport, "flags": flags, "count" : count}})
+
+def ip_checker():
+    # Geo Ip checker to check against dangerous countries
+    for ip in IPs:
+        response = requests.get(f'https://api.hackertarget.com/geoip/?q={ip}').content.decode().splitlines()[1].split(':', 1)[1].strip()
+        print(response)
+
+try:
+    if sys.argv[1] == "live":
+        liveCap()
+    if sys.argv[1] ==  "test":
+        ip_checker()
+except Exception as e:
+    if __name__ == "__main__":
+        main()
