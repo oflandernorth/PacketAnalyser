815bb847e5e310c7f3e8372383dcae1fac92c997
oflandernorth <oflandernorth@gmail.com>
2026-06-07 08:05:15 +0330

    Major Update

--- MonitoringTool.py
+++ MonitoringTool.py
@@ -1,19 +1,37 @@
 import sys
 import requests
 from scapy.all import *
+import sqlite3
+import json
 
 IPs = {}
-safelist = 0
+ip_observation = {}
+safelist = '.\safelist.txt'
+countries = '.\countries.txt'
+checkApi = ''
+conn = ''
+c = ''
 
 def main():  
-    global safelist
+    global safelist, countries, checkApi, conn, c
     print("Choose the type of operation you want to perform.")
     opType = input("live for live capture / read for capture file reading: ")
-    safelist = input("if you want to check against a list of safe ips enter the txt files address: ")
+    #safelist = input("if you want to check against a list of safe ips enter the txt files address: ")
     if safelist != 0:
         safelist = open(safelist)
         safelist = safelist.read()
         safelist = safelist.split("\n")
+    #countries = input("if you want to check against a list of unsafe countries enter the txt files address: ")
+    if countries != 0:
+        countries = open(countries)
+        countries = countries.read()
+        countries = countries.split("\n")
+    checkApi = input("if you want to check IPs against AbuseIPDB, enter the API key: ").strip()
+    conn = sqlite3.connect('PacketAnalyzer.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS ip_observations (
+                IP TEXT, Dport INTEGER, Protocol INTEGER, Country TEXT, Risk INTEGER)''')
+    conn.commit()
     match (opType):
         case "live":
             print("starting live capture")
@@ -27,6 +45,10 @@ def main():
             main()
             pass
     ip_checker()
+    c.execute("SELECT * FROM ip_observations")
+    for row in c.fetchall():
+        print(row)
+    conn.close()
 def liveCap():
     #intf = input("which interface to sniff on: ")
     intf = "Wi-Fi"
@@ -84,15 +106,50 @@ def packet_to_list(src, dst, proto, sport, dport, flags):
             IPs.update({src: {"proto": proto, "dport": dport, "flags": flags, "count" : count}})
 
 def ip_checker():
-    # Geo Ip checker to check against dangerous countries
+    global ip_observation, c
     for ip in IPs:
-        response = requests.get(f'https://api.hackertarget.com/geoip/?q={ip}').content.decode().splitlines()[1].split(':', 1)[1].strip()
-        print(response)
-
+        response = requests.get(f'https://api.hackertarget.com/geoip/?q={ip}').content.decode().split("\n")[1].split(':', 1)[1].strip()
+        ip_observation.update({ip: {"dport": IPs[ip]["dport"],  "proto": IPs[ip]["proto"], "country": response, "score": 0}})
+        # Check if the IP is from a dangerous country
+        if response in countries:
+            print(f"Packet from {ip} is from an unsafe country: {response}")
+            ip_observation[ip]["score"] += 5
+        else:
+            ip_observation[ip]["score"] += 0
+        # Check against abuseipdb database for reports of malicious activity
+        if checkApi != '':
+            querystring = {
+                'ipAddress': ip,
+                'maxAgeInDays': '90'
+            }
+            headers = {
+                'Accept': 'application/json',
+                'Key': checkApi,
+            }
+            response = requests.request(method='GET', url='https://api.abuseipdb.com/api/v2/check', headers=headers, params=querystring)
+            response = json.loads(response.text)
+            if response["data"]["totalReports"] > 5:
+                print(f"Packet from {ip} has been reported {response['data']['totalReports']} times in the last 90 days")
+                ip_observation[ip]["score"] += 10
+        # Check for repeated packets from the same IP
+        if IPs[ip]["count"] > 10:
+            print(f"Packet from {ip} has been observed {IPs[ip]['count']} times")
+            ip_observation[ip]["score"] += 15
+        # Check for common attack ports
+        if IPs[ip]["dport"] in [22, 23, 445,3389]:
+            print(f"Packet from {ip} is targeting a common attack port: {IPs[ip]['dport']}")
+            ip_observation[ip]["score"] += 20
+        for ip, data in ip_observation.items():
+            c.execute('''
+                INSERT INTO ip_observations (IP, Dport, Protocol, Country, Risk)
+                VALUES (?, ?, ?, ?, ?)
+                ON CONFLICT(IP) DO UPDATE SET Risk = Risk + 1
+            ''', (ip, data["dport"], data["proto"], data["country"], data["score"]))
+        conn.commit()
 try:
     if sys.argv[1] == "live":
         liveCap()
-    if sys.argv[1] ==  "test":
+    elif sys.argv[1] ==  "test":
         ip_checker()
 except Exception as e:
     if __name__ == "__main__":
